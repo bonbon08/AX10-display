febe0caf2035782f9d91282fcbed086aa0b26da3
bonbon08 <mail@jan-schmitt.eu>
2024-12-14 22:36:50 +0100

    added first test

---
+++ README.md
@@ -0,0 +1,4 @@
+# About this
+This is a Screen server for the AX10 emulator, it runs via pygame and connects to the AX10 emulator backend
+## Installation
+Please install all modules from the deb.txt
---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import socket
+
+def wait_on_connection():
+    pass
+
+def sync_screen():
+    pass
+
+class Screen():
+    def __init__(self):
+        pass
+    def clear(self):
+        pass
+    def set_pixel(self,x,y,rgb):
+        pass
+    def set_char(self,x,y,char):
+        pass
+
+if __name__ == "__main__":
+    pass
