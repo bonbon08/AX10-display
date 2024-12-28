0d86c56af1a5f4814952592d40df1869a8a1cbc2
bonbon08 <mail@jan-schmitt.eu>
2024-12-28 09:25:04 +0100

    changed so all runs in a single thread

--- main.py
+++ main.py
@@ -9,19 +9,7 @@ def wait_on_connection():
     print("Server waiting on incomig connection")
     conn, address = server_socket.accept()
     print("Connection established")
-    return conn, server_socket
-
-def sync_screen(conn, server_socket):
-    display = Screen()
-    window_thread = threading.Thread(target=display.window_main_loop, args=(), daemon=True)
-    while True:
-        commandslist = conn.recv(1024).decode("utf-8").split(" ")
-        if commandslist[0] == "0":
-            display.clear()
-        elif commandslist[0] == "1":
-            display.set_pixel(commandslist[1], commandslist[2], commandslist[3])
-    conn.close()
-    server_socket.close()
+    return [conn, server_socket]
 
 def hex_to_rgb(hex_code):
     r = int(hex_code[0:2], 16)
@@ -30,15 +18,12 @@ def hex_to_rgb(hex_code):
     return (r, g, b)
 
 class Screen():
-    def __init__(self):
-        self.disp = pygame.init()
-        pygame.display.set_mode((255, 255))
-        pygame.display.set_caption("AX-10 display")
-        self.clock = pygame.time.Clock()
     def clear(self):
         self.disp.fill("black")
+        pygame.display.flip()
     def set_pixel(self,x,y,rgb):
         self.disp.set_at((x, y), hex_to_rgb(rgb))
+        pygame.display.flip()
     def set_char(self,x,y,char):
         index1 = 0
         index2 = 0
@@ -49,16 +34,30 @@ class Screen():
                 index2 += 1
             index2 = 0
             index1 += 1
-    def window_main_loop(self):
-        run = True
-        while run:
+        pygame.display.flip()
+    def sync_screen(self, displiste):
+        self.disp = pygame.init()
+        pygame.display.set_mode((255, 255))
+        pygame.display.set_caption("AX-10 display")
+        self.clock = pygame.time.Clock()
+        conn = displiste[0]
+        server_socket = displiste[1]
+        display = Screen()
+        while True:
+            commandslist = conn.recv(1024).decode("utf-8").split(" ")
+            if commandslist[0] == "0":
+                display.clear()
+            elif commandslist[0] == "1":
+                display.set_pixel(commandslist[1], commandslist[2], commandslist[3])
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
                     print("Screen closed")
                     pygame.quit()
+                    break
             pygame.display.flip()
-            self.clock.tick(60)
+        conn.close()
+        server_socket.close()
 
 if __name__ == "__main__":
-    sync_screen(wait_on_connection())
+    screen = Screen()
+    screen.sync_screen(wait_on_connection())
